6a411693625deefcca68946c41ed791e22097159
Grant Steinfeld <grant.steinfeld.tech@gmail.com>
2022-08-13 08:17:23 +0000

    abandoning PSAdvantage for now

--- lib/StartPodeServer.ps1
+++ lib/StartPodeServer.ps1
@@ -27,19 +27,62 @@ Start-PodeServer -StatusPageExceptions Show {
     Add-PodeRoute -Method Post -Path '/api/record/:worksheetName' -ScriptBlock {
 
         $worksheetName = $WebEvent.Parameters['worksheetName']
-        $path = Join-Path GetTemporaryFileSytemPath $worksheetName
+        $localFileSytem = Join-Path GetTemporaryFileSytemPath $worksheetName
         $records = foreach ($item in $WebEvent.Data){
             [ PSCustomObject ] $item 
         }
 
-        #https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/new-temporaryfile?view=powershell-7.2
 
-        $TempFile = New-TemporaryFile
+        $records | Export-Excel -WorksheetName Log -TableName Log -Path $localFileSytem
 
-       $records | Export-Excel -WorksheetName Log -TableName Log -Path $TempFile
+       
+
+        $owner = 'agentidea'
+        $repo = "katagraphos-store"
+        $repoPath = "user_content/Clara.Pup/$worksheetName"
+
+        #Import-PSAdvantageConfig /home/grantsteinfeld/dev/katagraphos.net/api/katagraphos-api/lib/config/config.ps1 # imports config with GitHub Access Token
+
+        $token = "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"
+
+
+
+
+        #Excel is BLOB/ so - Encoding byte
+
+        #$excelFileName = "/Users/grantsteinfeld/Documents/dev/katagraphos.net/api/gh-api/salesData.xlsx"
+        $excelFileName = $localFileSytem
+        Write-Host $excelFileName
+        $content64 = [convert]::ToBase64String((Get-Content -path $excelFileName -AsByteStream))
+
+        $url = "https://api.github.com/repos/$owner/$repo/contents/$repoPath"
+
+
+        $jsonBody = @"
+        {
+        "message": "my commit message",
+        "committer": {
+            "name": "Grant Steinfeld",
+            "email": "grant.steinfeld.tech@gmail.com"
+        },
+        "content": "$content64"
+        }
+"@
+
+
+        $headers = @{
+        Accept = "application/vnd.github+json"
+        Authorization = "token $token"
+        }
+
+        Invoke-RestMethod -Method Put -Uri $url -Headers $headers -ContentType "application/json" -Body $jsonBody
+
+
+# https://zeleskitech.com/2016/03/10/building-json-powershell-objects/
+
      
 
-       Write-PodeJsonResponse -Value @{ 'dataStat' = $path; }
+       Write-PodeJsonResponse -Value @{ 'dataLocal-Stat' = $path; }
 
     }
 
